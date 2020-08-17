cb8cd5f54495e84a06107aa3b1f320d6593a2154
Dinu Radu Mihai <radurmx@gmail.com>
2020-08-17 16:01:15 +0300

    add connectionString

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : process.env.DAATABASE_URL, 
+    connectionString : process.env.DAATABASE_URL, 
     ssl: true,
   }
 });
