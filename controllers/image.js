1bffc09cdf57d33a9370fc7d370c5195d5f1db91
Dinu Radu Mihai <radurmx@gmail.com>
2020-08-24 13:40:29 +0300

    add envinronmental variable for API

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
- apiKey: '2021d01ab50b44728057b61097ce300c'
+ apiKey: process.env.API_CLARIFAI;
 });
 const handleApiCall = (req, res) => {
 	app.models
